aea63d1a459ff4bac7dc1fa2aa2faadecbdd175b
anelisevaz.anelise <anevaz1@gmail.com>
2022-01-14 19:27:59 -0300

    Teste

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1 align="center">Pasta PHP</h1>
+<p align="center">Mais detalhes sobre os projetos de cada pasta</p>
+
+<h3>Pasta: Curso | Instrutor | Observações</h3>
+<ul>
+  <li>Pasta 1: PHP 7 Completo - Curso do Desenvolvedor Web 2021 + Projetos | Leonardo Moura Leitao | O curso é focado no PHP 7, HTML, CSS, JavaScript, Banco de Dados, partindo dos fundamentos.</li>
+</ul>  
---
+++ pastaum/exercicio.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/estilo.css">
+    <title>Exercício</title>
+  </head>
+  <body>
+    <header class="cabecalho">
+      <h1>Curso PHP</h1>
+      <h2>Visualização dos Exercícios</h2>
+    </header>
+    <nav class="navegacao">
+      <a href="#" class="verde">Sem formatação</a>
+      <a href="index.php" class="vermelho">Voltar</a>
+    </nav>
+    <main class="principal">
+      <div class="conteudo">
+      </div>
+    </main>
+    <footer class="rodape">
+      ALUNA <?= date('Y') ?>
+    </footer>
+  </body>
+</html>
