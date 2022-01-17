c5dc821d51c9956be2fc372111e12f20a86cab42
anelisevaz.anelise <anevaz1@gmail.com>
2022-01-17 14:49:09 -0300

    Básico

--- basico/ola.php
+++ basico/ola.php
@@ -1,3 +1,20 @@
-<div class="titulo">Olá PHP</div>
+<div class="titulo">Básico</div>
 
-<?php  ?>
+<h4>Precedência das Operações Aritméticas:</h4>
+<p> () => ** => / * % => + - </p>
+<p><small>Obs.: Operador Exponencial **</small></p>
+<?php
+  echo 'Exemplo de precedência: ';
+  echo ((2+3)*4)**2;
+   ?>
+
+<hr>
+
+  <h4>String:</h4>
+  <?php
+    var_dump("Contando os caracteres");
+    echo "<br>Contatenando" . ' o texto';
+    print "<br>O print também imprime";
+   ?>
+
+   <h5>Algumas funções com string</h5>
